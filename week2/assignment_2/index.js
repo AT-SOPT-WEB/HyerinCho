2be6801059f0afb1506f54547dac00e78eceb605
jogpfls <joerin4177@gmail.com>
2025-04-25 19:30:43 +0900

    feat: TodoList 추가 및 삭제 구현

--- week2/assignment_2/index.js
+++ week2/assignment_2/index.js
@@ -1,13 +1,21 @@
 import { todos } from "./data.js";
 
-if (!localStorage.getItem('todoData') || []) {
+const data = JSON.parse(localStorage.getItem('todoData')).length;
+if (!localStorage.getItem('todoData') || data == 0) {
   localStorage.setItem('todoData', JSON.stringify(todos));
 }
 
 const table =  document.querySelector('.todo-table');
-let todoData = JSON.parse(localStorage.getItem('todoData'));
+const input = document.querySelector('.todo-value');
+const addTodoBtn = document.querySelector('.todo-add-btn');
+const priority = document.querySelector('select[name=choose-priority]');
+const tableHeader = table.querySelector('.todo-header').cloneNode(true);
+const deleteBtn = document.querySelector('.todo-delete');
 
-todoData.forEach(todo => {
+let todoData = JSON.parse(localStorage.getItem('todoData')) || [];
+
+//전체 Todo 출력
+const showAllTodo = (todo) => {
   const tr = document.createElement('tr');
   tr.innerHTML = `
     <td><input class="todo-check" type="checkbox"></td>
@@ -16,5 +24,54 @@ todoData.forEach(todo => {
     <td class="todo-title">${todo.title}</td>
   `;
   table.appendChild(tr);
-});
+}
+
+todoData.forEach((todo)=>{
+  showAllTodo(todo)
+})
+
+//테이블 다시 그리기
+const renderTable = () => {
+  table.innerHTML = '';
+  table.appendChild(tableHeader.cloneNode(true));
+  todoData.forEach(todo => showAllTodo(todo));
+}
+
+// Todo 추가
+addTodoBtn.addEventListener('click', () => {
+  const inputValue = input.value;
+  const priorityValue = priority.value;
+  
+  if (!inputValue || !priorityValue){
+    alert('할 일과 중요도 모두 입력해주세요 !')
+    return
+  };
+
+  const newTodo = {
+    id: todoData.length + 1,
+    title: inputValue,
+    completed: false,
+    priority: priorityValue
+  }
+  
+  todoData.push(newTodo)
+  localStorage.setItem('todoData', JSON.stringify(todoData));
+
+  input.value = '';
+  showAllTodo(newTodo)
+})
+
+// Todo 삭제
+deleteBtn.addEventListener('click', () => {
+  const checkedTodo = document.querySelectorAll('.todo-check:checked');
+
+  checkedTodo.forEach((check) => {
+    const tr = check.closest('tr');
+    const title = tr.querySelector('.todo-title').textContent;
+    todoData = todoData.filter(todo => todo.title !== title);
+  })
+
+  localStorage.setItem('todoData', JSON.stringify(todoData));
+  renderTable();
+})
 
