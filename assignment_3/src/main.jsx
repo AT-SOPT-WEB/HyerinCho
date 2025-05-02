a91a826236e2a8dc8ade023d9dafb233d11eb4f4
jogpfls <joerin4177@gmail.com>
2025-05-02 16:26:40 +0900

    set: global, theme 설정

--- assignment_3/src/main.jsx
+++ assignment_3/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Global, ThemeProvider } from "@emotion/react";
+import GlobalStyle from "./styles/GlobalStyle.js";
+import { theme } from './styles/Theme.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyle}/>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ assignment_3/src/styles/Theme.js
@@ -0,0 +1,8 @@
+
+export const theme = {
+  color: {
+    blue100: "#E9F0FA",
+    blue200: "#3D72AE",
+    blue300:"#182132",
+  }
+}
