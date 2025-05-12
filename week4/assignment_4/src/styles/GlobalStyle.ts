c718ae339f1cbd7cab0bde72e65c96be19f8b6c0
jogpfls <joerin4177@gmail.com>
2025-05-13 01:13:00 +0900

    init: 초기 설정 수정

--- week4/assignment_4/src/styles/GlobalStyle.ts
+++ week4/assignment_4/src/styles/GlobalStyle.ts
@@ -1,19 +1,20 @@
 import { css } from '@emotion/react';
+import { theme } from './Theme';
 
 const GlobalStyle = () => css`
     html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
-    a, dl, dt, dd, ol, ul, li, form, label, table, textarea, input{
+    a, dl, dt, dd, ol, ul, li, form, label, table, textarea, input{   
         margin: 0;
         padding: 0;
         border: 0;
         text-decoration: none;
         box-sizing: border-box;
         &:visited{
-        text-decoration: none;
         color: black;
         }
     }
     body{
+        background-color: ${theme.color.gray100};
         line-height: 1;
         margin: 0;
         padding: 0;
